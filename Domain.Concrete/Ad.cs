09581a77fbe2234734eaedf6caff197610e2bf13
André Steenhoff Madsen <andre@steenhoff.dk>
2018-11-08 09:49:32 +0100

    Changed our domain
    Added more properties to our domain, both in model and code

--- Domain.Concrete/Ad.cs
+++ Domain.Concrete/Ad.cs
@@ -10,10 +10,20 @@ namespace Domain.Concrete
     public class Ad : IAd
     {
         public string AdName { get; set; }
-        public string FilePath { get; set; }
+        public string FileLocation { get; set; }
         public DateTime CreationDate { get; set; }
         public string FileExtension { get; set; }
         public ICollection<IClient> Clients { get; set; }
         public int Id { get; set; }
+        public TimeSpan TotalPlayTime { get; set; }
+        public int TotalPlayCount { get; set; }
+
+        public string CompleteFilePath { get { return string.Format("{0}{1}{2}", FileLocation, AdName, FileExtension); } }
+        public int ClientsCount { get { return Clients.Count; } }
+
+        public Ad()
+        {
+            Clients = new List<IClient>();
+        }
     }
 }
