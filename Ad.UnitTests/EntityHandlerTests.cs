acd14d1113cc1b05c32f02a05038751eaed8655f
andr9528 <andre@steenhoff.dk>
2018-11-23 12:28:44 +0100

    force injecting test project so that it should cooperate again

---
+++ Ad.UnitTests/EntityHandlerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ad.UnitTests
+{
+    [TestClass]
+    public class EntityHandlerTests : BaseForTests
+    {
+        [TestMethod]
+        public void CreateAd_SuccesfullyAdd()
+        {
+            
+        }
+        [TestMethod]
+        public void CreatePlaylist_SuccesfullyAdd()
+        {
+            
+        }
+        [TestMethod]
+        public void CreateClient_SuccesfullyAdd()
+        {
+            
+        }
+        [TestMethod]
+        public void GetAd_SuccesfullyGet()
+        {
+            
+        }
+        [TestMethod]
+        public void GetPlaylist_SuccesfullyGet()
+        {
+            
+        }
+        [TestMethod]
+        public void GetClient_SuccesfullyGet()
+        {
+            
+        }
+        [TestMethod]
+        public void UpdateAd_SuccesfullyUpdate()
+        {
+            
+        }
+        [TestMethod]
+        public void UpdatePlaylist_SuccesfullyUpdate()
+        {
+            
+        }
+        [TestMethod]
+        public void UpdateClient_SuccesfullyUpdate()
+        {
+            
+        }
+        [TestMethod]
+        public void DeleteAd_SuccesfullyDelete()
+        {
+            
+        }
+        [TestMethod]
+        public void DeletePlaylist_SuccesfullyDelete()
+        {
+            
+        }
+        [TestMethod]
+        public void DeleteClient_SuccesfullyDelete()
+        {
+            
+        }
+        [TestMethod]
+        public void OnlyAllowUniqueOneCreateAd_FailedAdd()
+        {
+            
+        }
+        [TestMethod]
+        public void OnlyAllowUniqueOneCreatePlaylist_FailedAdd()
+        {
+            
+        }
+        [TestMethod]
+        public void OnlyAllowUniqueOneCreateClient_FailedAdd()
+        {
+            
+        }
+    }
+}
---
+++ Ad.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ad.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ad.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5970c675-73ee-4758-927a-18018af63437")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
