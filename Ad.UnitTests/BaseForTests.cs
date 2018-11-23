96ea568acffeb63d1b3ddb5487cc5a6ed40d7255
andr9528 <andre@steenhoff.dk>
2018-11-23 11:26:33 +0100

    commiting everything before trying to pull in ObjectGenerator

---
+++ Ad.UnitTests/BaseForTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repository.Core;
+using Repository.EntityFramework;
+
+namespace Ad.UnitTests
+{
+    
+    public class BaseForTests
+    {
+        private readonly string connectionPath = "..\\..\\ConnectionStringFile.txt";
+        private string connectionString = "";
+        internal IRepository handler;
+
+        [TestInitialize]
+        public void Init()
+        {
+            handler = new EntityRepositoryHandler(connectionString: GetConnectionString());
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+
+        }
+
+        private string GetConnectionString()
+        {
+            if (string.IsNullOrEmpty(connectionString))
+                connectionString = File.ReadLines(connectionPath).First();
+            return connectionString;
+        }
+    }
+}
