964136773ca80aeeeba03564719509e77d48d599
ChristianTheBob <chri894u@edu.eal.dk>
2018-12-19 10:17:18 +0100

    Upload File
    Work done to upload a video file from the web UI.

--- Ad.Website/Pages/Index.cshtml.cs
+++ Ad.Website/Pages/Index.cshtml.cs
@@ -1,22 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ad.Website.Pages
 {
-
     public class IndexModel : PageModel
     {
-
         public void OnGet()
         {
-            ServerService.ServerServiceClient bob = new ServerService.ServerServiceClient();
-          //  bob.UploadFileAsync
         }
 
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            bool success = false;
+            ServerService.ServerServiceClient serverService = new ServerService.ServerServiceClient();
+
+            long size = files.Sum(f => f.Length);
+            var filePath = Path.GetTempFileName();
+        
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //byte[] addBytes = new byte[stream.Length];
+                        //await serverService.SetNextFileNameWithoutOverrideAsync(filePath);
+                        //stream.Read(addBytes, 0, addBytes.Length);
+                        //success = await serverService.UploadFileAsync(addBytes);
+                        //await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            if (success == true)
+            {
+
+            }
+            else
+            {
+
+            }
+            //var file = new { count = files.Count, size, filePath };
+            return null;
+        }
     }
 }
